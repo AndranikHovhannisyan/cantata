b8cfd7aa821e8ea75ef6ba50eff87ded3b6e34f0
Hazarapet <hazarapet@hazarapet.(none)>
2014-05-18 19:07:29 +0400

    p

--- src/Cantata/MainBundle/Resources/public/js/app_pre.js
+++ src/Cantata/MainBundle/Resources/public/js/app_pre.js
@@ -1,3 +1,19 @@
 'use strict';
 
-angular.module('prefix', []);
+angular.module('prefix', [], ['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push(function() {
+        return {
+            'request': function(config) {
+                if(config.url.indexOf(".html") === -1){
+                    config.url = '/app.php' + config.url;
+                }
+                return config;
+            },
+
+            'response': function(response) {
+            // same as above
+                return response;
+            }
+        };
+    });
+}]);
